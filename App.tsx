bc24e59df284964be94aafd53dd982d233f27a5e
Ravindersharma83 <ravinder200198@gmail.com>
2025-01-10 15:13:24 +0530

    Setting up Drawer Navigation React native

--- App.tsx
+++ App.tsx
@@ -1,36 +1,73 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import React from 'react';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import HomeScreen from './src/Screens/HomeScreen';
+// import AboutScreen from './src/Screens/AboutScreen';
+
+// const Stack = createNativeStackNavigator();
+
+// const App = () => {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen 
+//           name="Home" 
+//           component={HomeScreen} 
+//           options={{ 
+//             title: 'Home Page',
+//             headerStyle: {
+//               backgroundColor: '#f4511e',
+//             },
+//             headerTintColor: '#fff',
+//             headerTitleStyle: {
+//               fontWeight: 'bold',
+//             },
+//           }} 
+//         />
+//         <Stack.Screen name="About" component={AboutScreen} options={{ title: 'About Page' }} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   )
+// }
+
+// export default App
+
+// const styles = StyleSheet.create({})
+
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './src/Screens/HomeScreen';
-import AboutScreen from './src/Screens/AboutScreen';
 
-const Stack = createNativeStackNavigator();
-
-const App = () => {
+function HomeScreen({ navigation }:any) {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen} 
-          options={{ 
-            title: 'Home Page',
-            headerStyle: {
-              backgroundColor: '#f4511e',
-            },
-            headerTintColor: '#fff',
-            headerTitleStyle: {
-              fontWeight: 'bold',
-            },
-          }} 
-        />
-        <Stack.Screen name="About" component={AboutScreen} options={{ title: 'About Page' }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
 }
 
-export default App
+function NotificationsScreen({ navigation }:any) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
 
-const styles = StyleSheet.create({})
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
